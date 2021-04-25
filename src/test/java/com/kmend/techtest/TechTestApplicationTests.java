f8d02044ead9bd834c636dfc5141ac46a390c9c1
Dwayne Patel <dwaynepatel@hotmail.com>
2021-04-25 10:06:12 +0100

    initial commit app running

---
+++ src/test/java/com/kmend/techtest/TechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmend.techtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
